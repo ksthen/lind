8ade07331e77b660e0677e78324afb88bf3a2eb4
Kristoffer Sthen <kristoffer@sthen.se>
2018-05-16 21:49:44 +0200

    tog bort effekt på file-list

--- src/app/components/file-list/file-list.component.ts
+++ src/app/components/file-list/file-list.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, Input, HostListener, Inject, 
-  PLATFORM_ID, ElementRef } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-import { staggerFade } from '../../app.animations';
+import { Component, OnInit, Input } from '@angular/core';
 
 export interface FileList {
   header: string;
@@ -17,42 +14,14 @@ export interface File {
 @Component({
   selector: 'app-file-list',
   templateUrl: './file-list.component.html',
-  styleUrls: ['./file-list.component.scss'],
-  animations: [staggerFade],
+  styleUrls: ['./file-list.component.scss']
 })
 export class FileListComponent implements OnInit {
 
   @Input() files: FileList;
 
+  constructor() { }
 
-  public state: string;
-
-  @HostListener('window:scroll', ['$event'])
-  checkScroll() {
-    if (isPlatformBrowser(this.platformId)) {
-      const componentPosition = this.el.nativeElement.offsetTop;
-      const scrollPosition = window.pageYOffset;
-      const windowHeight = window.innerHeight;
-      const offset = 400;
-
-      if (scrollPosition + windowHeight - offset >= componentPosition) {
-        this.state = 'show';
-      } else if (scrollPosition + windowHeight - offset * 2 < componentPosition) {
-        this.state = 'hide';
-      }
-    }
-  }
-
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: any, 
-    private el: ElementRef) { }
-
-  ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.state = 'hide';
-    } else {
-      this.state = 'show';
-    }
-  }
+  ngOnInit() {}
 
 }
